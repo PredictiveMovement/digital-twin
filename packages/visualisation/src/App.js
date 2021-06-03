12c3285d7665709639a0efd0e38599d439825278
izabellalarsson <izzwii.izabella@gmail.com>
2021-06-03 15:48:27 +0200

    clean up

--- packages/visualisation/src/App.js
+++ packages/visualisation/src/App.js
@@ -2,21 +2,6 @@ import React from 'react'
 import Map from './Map.js'
 import { useSocket } from './hooks/useSocket'
 
-const DELAY_MS = 15000
-
-function interpolatePositionFromTail(tail, time) {
-  const next = tail.filter((point) => point[2] > time)[0]
-  const current = tail[tail.indexOf(next) - 1]
-  if (!current || !next) return next
-  const progress = (time - current[2]) / (next[2] - current[2])
-  const speed = Math.round(current[3])
-  return [
-    current[0] + (next[0] - current[0]) * progress,
-    current[1] + (next[1] - current[1]) * progress,
-    time,
-    speed,
-  ]
-}
 const App = () => {
   const [postombud, setPostombud] = React.useState({
     type: 'FeatureCollection',
@@ -27,10 +12,11 @@ const App = () => {
     type: 'FeatureCollection',
     features: [],
   })
-  const [poll, setPoll] = React.useState(null)
-
   useSocket('postombud', (newPostombud) => {
     const features = [
+      ...postombud.features.filter(
+        (ombud) => !newPostombud.some((np) => np.id === ombud.id)
+      ),
       ...newPostombud.map(({ position }) => ({
         type: 'Feature',
         geometry: { type: 'Point', coordinates: [position[0], position[1]] },
@@ -51,21 +37,6 @@ const App = () => {
     ]
     setCars(Object.assign({}, cars, { features }))
   })
-
-  const interpolate = () => {
-    setCars(
-      Object.assign({}, cars, {
-        features: cars.features.map(
-          (car) =>
-            (car.coordinates =
-              interpolatePositionFromTail(car.tail, Date.now() - DELAY_MS) ||
-              car.coordinates)
-        ),
-      })
-    )
-    clearTimeout(poll)
-    setPoll(setTimeout(() => interpolate(), 500))
-  }
 
   return (
     <>
