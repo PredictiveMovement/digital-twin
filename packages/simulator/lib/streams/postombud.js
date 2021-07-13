19715112c8771c1459666f7c4403363172f08200
izabellalarsson <izzwii.izabella@gmail.com>
2021-07-13 15:13:02 +0200

    fix: the start script and update filename

--- packages/simulator/lib/streams/postombud.js
+++ packages/simulator/lib/streams/postombud.js
@@ -2,20 +2,20 @@
  * List of hubs from postombud ljusdal
  */
 
-const fs = require("fs");
-const XLSX = require("xlsx");
-const {from} = require('rxjs');
-const { filter, take } = require("rxjs/operators");
+const fs = require('fs');
+const XLSX = require('xlsx');
+const { from } = require('rxjs');
+const { filter, take } = require('rxjs/operators');
 
 const readXlsx = (path, sheet) => {
   const buf = fs.readFileSync(path);
-  const wb = XLSX.read(buf, { type: "buffer" });
+  const wb = XLSX.read(buf, { type: 'buffer' });
 
   return XLSX.utils.sheet_to_json(wb.Sheets[sheet]);
 };
 
 const postombud = readXlsx(
-  `${process.cwd()}/data/Ombudsnät2021.xlsx`,
+  `${process.cwd()}/data/ombudsnat.xlsx`,
   `Sammanställning korr`
 ).map(({ X_WGS84, Y_WGS84, LevFrekv, OPERATÖR, DB_ID, KOMMUNNAMN }) => ({
   position: { lon: parseFloat(X_WGS84, 10), lat: parseFloat(Y_WGS84, 10) },
@@ -23,7 +23,7 @@ const postombud = readXlsx(
   frequency: LevFrekv,
   id: DB_ID,
   kommun: KOMMUNNAMN,
-}))
+}));
 
 // module.exports = from(postombud)
 //   .pipe(
@@ -31,4 +31,4 @@ const postombud = readXlsx(
 //     // take(2)
 //   )
 
-module.exports = postombud.filter(f => f.kommun === 'Ljusdal')
+module.exports = postombud.filter((f) => f.kommun === 'Ljusdal');
