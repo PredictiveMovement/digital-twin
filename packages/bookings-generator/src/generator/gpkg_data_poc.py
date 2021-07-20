0ac40f346e7cd84b612708d279fdbc4ee52f4100
izabellalarsson <izabella.larsson@bambuser.com>
2021-07-20 12:54:00 +0000

    chore: filter csv to get info from ljusdal area
    Co-authored-by: Radu Achim <raduachim@users.noreply.github.com>
    Co-authored-by: Adam Näslund <iteadam@users.noreply.github.com>

--- packages/bookings-generator/src/generator/gpkg_data_poc.py
+++ packages/bookings-generator/src/generator/gpkg_data_poc.py
@@ -1,6 +1,16 @@
 import fiona
 
 
+def _is_in_bounds(polygon):
+    # this is the area around Ljusdal in SWEREF99TM
+    # upper left N 6869841.085 , E 537429.637
+    # lower right N 6832795.482 , E 588303.781
+    for point in polygon:
+        if 537429.637 < point[0] < 588303.781 and 6832795.482 < point[1] < 6869841.085:
+            return True
+    return False
+
+
 def load():
     source_fileName = "data/raw/Totalbefolkning_1km_191231.gpkg"
     destination_filename = "data/raw/Totalbefolkning_1km_191231_updated.gpkg"
@@ -21,10 +31,15 @@ def load():
 
         print("=====> writing destination gpkg file")
 
+        count = 0
         with fiona.open(destination_filename, "w", driver, schema, crs) as destination:
             for feature in source:
+                if not _is_in_bounds(feature["geometry"]["coordinates"][0]):
+                    continue
+                count += 1
                 population = feature["properties"]["pop"]
                 feature["properties"]["packages"] = population / 10
                 destination.write(feature)
+        print(f"Found {count} matching entries")
 
     print("=====> completed writing destination gpkg file")
