3d8f27832afec524728071059247ad842f4fa651
Christian Landgren <christian@landgren.nu>
2021-08-09 01:16:07 +0200

    feat: bugfix bookings

--- packages/simulation-engine/simulator/dispatchCentral.js
+++ packages/simulation-engine/simulator/dispatchCentral.js
@@ -7,12 +7,13 @@ const dispatch = (cars, bookings) => {
     //tap(booking => console.log(`*** new booking ${booking.id}. Looking for a car nearby...`)),
     concatMap((booking) => cars.pipe(
       map((car) => ({car, distance: haversine(car.position, booking.pickup.position)})),
-      takeUntil(timer(500)), // to be able to sort we have to batch somehow. Lets start with time
+      filter(({car}) => car.capacity > car.queue.length), // wait until we have a car with free capacity
+      takeUntil(timer(100)), // to be able to sort we have to batch somehow. Lets start with time
       toArray(),
       //tap(cars => console.log(`*** available cars to choose from: ${cars.length}`)),
       // naive dispatch, just pick the first car that is closest to the pickup
       map((cars) => cars.sort((a, b) => a.distance - b.distance).shift()?.car),
-      filter(car => car), // wait until we have a car
+      filter(car => car),
       map(car => ({car, booking: car.handleBooking(booking)})),
       //tap(({car, booking}) => console.log(`*** booking ${booking.id} dispatched to car #${car.id}`)),
     ))
