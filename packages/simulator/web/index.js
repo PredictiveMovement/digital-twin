e175434db31ca86c8c2b16dff1f2d4e0932fd916
Emil Ahlbäck <emil.ahlback@iteam.se>
2021-08-10 14:12:09 +0200

    feat: Add cors

--- packages/simulator/web/index.js
+++ packages/simulator/web/index.js
@@ -4,6 +4,14 @@ const routes = require('./routes')
 const port = 4000
 
 const server = require('http').createServer()
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, {
+  // default cros package
+  cors: {
+    "origin": "*",
+    "methods": "GET,HEAD,POST", // not default
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }
+})
 server.listen(port)
 routes.register(io)
