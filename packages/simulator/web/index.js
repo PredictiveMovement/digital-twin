cd4a223a702d83404dbed2de98b730212305d38a
Christian Landgren <christian@landgren.nu>
2021-08-10 14:17:29 +0200

    chore: use default options for cors

--- packages/simulator/web/index.js
+++ packages/simulator/web/index.js
@@ -5,12 +5,9 @@ const port = 4000
 
 const server = require('http').createServer()
 const io = require('socket.io')(server, {
-  // default cros package
   cors: {
-    "origin": "*",
-    "methods": "GET,HEAD,POST", // not default
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
+    origin: "*",
+    methods: ["GET", "POST"]
   }
 })
 server.listen(port)
