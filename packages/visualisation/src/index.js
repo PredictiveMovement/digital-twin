522e89aebc8bbee8f96221983161b2d689cbbc81
Christian Landgren <christian@landgren.nu>
2021-08-10 12:05:31 +0200

    chore: add mapbox token and server-url

--- packages/visualisation/src/index.js
+++ packages/visualisation/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { SocketIOProvider } from "./context/socketIOProvider";
 
 ReactDOM.render(
-  <SocketIOProvider url="http://localhost:4000">
+  <SocketIOProvider url={process.env.REACT_APP_SIMULATOR_URL}>
     <App />
   </SocketIOProvider>,
   document.getElementById("root")
@@ -15,4 +15,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
