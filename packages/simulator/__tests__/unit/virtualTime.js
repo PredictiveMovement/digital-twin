2888c1b0bb093d547353052eab44c3f438afe504
Alexander Czigler <alexander@iteam.se>
2021-08-24 17:05:28 +0200

    fix: virtualTime

---
+++ packages/simulator/__tests__/unit/virtualTime.js
@@ -0,0 +1,53 @@
+const { italic } = require('chalk')
+const { VirtualTime } = require('../../lib/virtualTime')
+
+expect.extend({
+  toBeNear(x, y, z) {
+    return {
+      pass: Math.round(x / 100) === Math.round(y / 100),
+      message: () => `Not close enough: expected: ${x}, received: ${y}. Diff: ${x - y}`,
+    }
+  },
+})
+
+describe('VirtualTime', () => {
+  let virtualTime
+
+  beforeEach(() => {
+    virtualTime = new VirtualTime(1)
+  })
+
+  it('can pass the time', (done) => {
+    let start = virtualTime.time()
+
+    setTimeout(() => {
+      expect(virtualTime.time()).toBeNear(start + 1000)
+      done()
+    }, 1000)
+  })
+
+  it('can pause and receive same time', (done) => {
+    let start = virtualTime.time()
+    virtualTime.pause()
+
+    setTimeout(() => {
+      expect(virtualTime.time()).toBeNear(start)
+      done()
+    }, 1000)
+  })
+
+  it('can pause and resume and receive same time plus extra time', (done) => {
+    let start = virtualTime.time()
+    virtualTime.pause()
+
+    setTimeout(() => {
+      expect(virtualTime.time()).toBeNear(start)
+      virtualTime.play()
+
+      setTimeout(() => {
+        expect(virtualTime.time()).toBeNear(start + 1000)
+        done()
+      }, 1000)
+    }, 1000)
+  })
+})
--- packages/simulator/lib/virtualTime.js
+++ packages/simulator/lib/virtualTime.js
@@ -6,40 +6,36 @@ class VirtualTime extends EventEmitter {
 
   constructor(timeMultiplier = 60) {
     super()
-    this.timeMultiplier = timeMultiplier
-    this.startDate = Date.now()
-    this.offset = 0
+    this.setTimeMultiplier(timeMultiplier)
   }
 
-  time () {
+  time() {
     const diff = Date.now() - this.startDate
-    return Date.now() + this.offset + diff * this.timeMultiplier
+    return Date.now() + diff * this.timeMultiplier
   }
 
   play() {
-    console.log('Play', new Date(this.time()))
-    this.setTimeMultiplier(this.oldVirtualTime)
-    this.offset = Date.now() - this.startDate
+    this.setTimeMultiplier(this.oldTimeMultiplier)
     this.emit('play')
   }
 
   pause() {
-    console.log('Pause', new Date(this.time()))
-    this.oldVirtualTime = this.timeMultiplier
+    this.oldTimeMultiplier = this.timeMultiplier
     this.startDate = Date.now()
     this.setTimeMultiplier(0) // we let time pass but compensate for each second with one minus second
     this.emit('pause')
   }
-  
+
   setTimeMultiplier(timeMultiplier) {
     this.startDate = Date.now()
-    this.timeMultiplier = timeMultiplier
+    this.timeMultiplier = timeMultiplier - 1 // it makes more sense to have 1 mean realtime and 0 means stop the time.
   }
-  
+
 }
 
 
 
-module.exports = { 
-  virtualTime: new VirtualTime() // static global time
+module.exports = {
+  virtualTime: new VirtualTime(), // static global time
+  VirtualTime
 }
