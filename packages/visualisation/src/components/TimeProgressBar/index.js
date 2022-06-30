82fe4686bc4372004fd8dd8869934f14cbe35c46
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2022-06-30 16:11:01 +0200

    styling:  make progress bar bigger on hover

--- packages/visualisation/src/components/TimeProgressBar/index.js
+++ packages/visualisation/src/components/TimeProgressBar/index.js
@@ -1,15 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Wrapper = styled.div`
-  position: absolute;
-  bottom: 2.5rem;
-  left: 15rem;
-  height: 50px;
-  display: flex;
-  align-items: center;
-`
-
 const Container = styled.div`
   height: 8px;
   width: 70vw;
@@ -25,12 +16,6 @@ const Filler = styled.div`
   height: 2px;
 `
 
-const Total = styled.div`
-  width: ${({ completed }) => `${completed}%`};
-  height: 2px;
-  border-radius: 6px;
-`
-
 const Circle = styled.div`
   height: 10px;
   width: 10px;
@@ -41,6 +26,28 @@ const Circle = styled.div`
   left: ${({ completed }) => `${completed - 0.3}%`};
 `
 
+const Wrapper = styled.div`
+  position: absolute;
+  bottom: 2.5rem;
+  left: 15rem;
+  height: 50px;
+  display: flex;
+  align-items: center;
+
+  &:hover ${Filler} {
+    height: 8px;
+  }
+  &:hover ${Container} {
+    height: 8px;
+  }
+  &:hover ${Circle} {
+    height: 30px;
+    width: 30px;
+    left: ${({ completed }) => `${completed - 1.2}%`};
+    top: 8px;
+  }
+`
+
 const ProgressBar = ({ time }) => {
   const totalMinutes =
     new Date(time).getHours() * 60 + new Date(time).getMinutes()
@@ -48,11 +55,10 @@ const ProgressBar = ({ time }) => {
   const completed = (totalMinutes / 1440) * 100
 
   return (
-    <Wrapper>
+    <Wrapper completed={completed}>
       <Container>
         <Filler completed={completed}>
           {completed !== 100 && <Circle completed={completed} />}
-          <Total completed={completed} />
         </Filler>
       </Container>
     </Wrapper>
