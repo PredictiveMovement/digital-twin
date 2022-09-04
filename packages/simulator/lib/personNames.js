54ccf72e99d4e2e342833f96a0e2e8f0ac1483f2
Christian Landgren <christian@landgren.nu>
2022-09-04 15:56:39 +0200

    feat: generate realistic names including last names

--- packages/simulator/lib/personNames.js
+++ packages/simulator/lib/personNames.js
@@ -1,21 +1,43 @@
-const { from, repeat, map, zip, take, filter } = require('rxjs')
+const {
+  from,
+  repeat,
+  map,
+  zip,
+  take,
+  filter,
+  sample,
+  debounce,
+  toArray,
+  pipe,
+  mergeAll,
+} = require('rxjs')
 const fornamn = require('../data/svenska_tilltalsnamn_2021.json').data
 const efternamn = require('../data/svenska_efternamn_2021.json').data
 
+const shuffle = () =>
+  pipe(
+    toArray(),
+    map((names) => names.sort(() => Math.random() - 0.5)), // shuffle the names
+    mergeAll()
+  )
+
+const zipfDistribution = () =>
+  pipe(
+    map((name, i) => ({
+      name,
+      frequency: 1 / (i + 1), // Zipf distribution
+    }))
+  )
 /**
- * Takes a list of names sorted on frequency of use
+ * Takes a distribution of names with frequency of use
  * and returns a stream of names according to the distribution
  * - meaning that the first name is more likely to be used than the second
  *
  * @returns stream of names
  */
-const weightedRandom = () => (distribution) =>
-  from(distribution).pipe(
-    map((name, i) => ({
-      name,
-      frequency: 3 / (i + 1), // Zipf distribution
-    })),
-    filter(({ frequency }) => frequency > Math.random() / 2),
+const weightedRandom = () =>
+  pipe(
+    filter(({ frequency }) => frequency > Math.random()),
     map(({ name }) => name)
   )
 
@@ -25,23 +47,28 @@ const toToTitleCase = (str) =>
     .map((word) => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase())
     .join(' ')
 
-const randomFirstName = () => from(fornamn).pipe(weightedRandom())
+const randomFirstName = () =>
+  from(fornamn).pipe(zipfDistribution(), shuffle(), weightedRandom())
 
 const randomLastName = () =>
   from(efternamn).pipe(
+    zipfDistribution(),
+    shuffle(),
     weightedRandom(),
     map((name) => toToTitleCase(name))
   )
 
-const generateNames = () =>
-  zip(randomFirstName(), randomLastName()).pipe(
-    map(([firstName, lastName]) => ({
-      firstName,
-      lastName,
-      name: `${firstName} ${lastName}`,
-    }))
-  )
+const randomNames = zip(randomFirstName(), randomLastName()).pipe(
+  map(([firstName, lastName]) => ({
+    firstName,
+    lastName,
+    name: `${firstName} ${lastName}`,
+  })),
+  repeat()
+)
 
 module.exports = {
-  generateNames,
+  randomNames,
 }
+
+// randomNames.pipe(take(10)).subscribe(console.log)
