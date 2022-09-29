ab059fdce4711dac5f937e6f57331d30cd2b4933
Mikael Gråborg <mikael.graborg@iteam.se>
2022-09-29 15:09:34 +0200

    feat: add measureStations

---
+++ packages/simulator/streams/measureStations.js
@@ -0,0 +1,34 @@
+const { from, shareReplay } = require('rxjs')
+const { map, filter } = require('rxjs/operators')
+const { readXlsx } = require('../adapters/xlsx')
+const sweCoords = require('swe-coords')
+
+function execute() {
+  return from(
+    readXlsx(
+      `${process.cwd()}/data/helsingborg/${
+        process.env.transports_file || 'transports2021.xlsx'
+      }`,
+      `${process.env.transports_sheet || 'Blad1'}`
+    )
+  )
+    .pipe(
+      map(
+        ({
+          x,
+          y,
+          // 'HBG nr': hbgNr,
+          // 'Traffic Web': trafficWeb,
+          // Mätår: year,
+          // Månad: month,
+          // 'VaDT Tung': heavyTraficCount,
+        }) => ({
+          position: sweCoords.toLatLng(x, y),
+        })
+      ),
+      shareReplay()
+    )
+    .subscribe(console.log)
+}
+
+module.exports = execute()
