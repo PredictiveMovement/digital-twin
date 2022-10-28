59db8ed97893e835f8f97d1661136f4526150928
Christian Landgren <christian@landgren.nu>
2022-10-28 07:22:07 +0200

    chore: optimize load time, send all busstops at once

--- packages/simulator/web/routes/buses.js
+++ packages/simulator/web/routes/buses.js
@@ -1,11 +1,14 @@
+const { toArray } = require('rxjs')
+
 const register = (experiment, socket) => {
   return [
-    experiment.busStops.subscribe((busStops) =>
-      socket.emit('busStops', busStops)
-    ),
-    experiment.lineShapes.subscribe((lineShapes) =>
-      socket.emit('lineShapes', lineShapes)
-    ),
+    experiment.busStops
+      .pipe(toArray())
+      .subscribe((busStops) => socket.emit('busStops', busStops)),
+
+    experiment.lineShapes
+      .pipe(toArray())
+      .subscribe((lineShapes) => socket.emit('lineShapes', lineShapes)),
   ]
 }
 
