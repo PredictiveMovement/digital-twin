6796fdb0593fc862c5ee3eec00bf4dfabcf39872
Christian Landgren <christian@landgren.nu>
2022-10-28 17:51:55 +0200

    feat: add bookings to passengers in map

--- packages/simulator/web/routes/passengers.js
+++ packages/simulator/web/routes/passengers.js
@@ -1,3 +1,5 @@
+const { startWith, mergeMap, scan, map } = require('rxjs')
+
 /**
  * When a new socket is connected, we send the current state of the experiment
  *
@@ -7,9 +9,21 @@
  */
 const register = (experiment, socket) => {
   return [
-    experiment.passengers.subscribe((passenger) =>
-      socket.emit('passenger', passenger.toObject())
-    ),
+    experiment.passengers
+      .pipe(
+        mergeMap((passenger) =>
+          passenger.bookings.pipe(
+            scan((acc, booking) => [...acc, booking.toObject()], []), // every booking is added to the array
+            map((bookings) => ({ passenger, bookings })),
+            startWith(
+              { passenger, bookings: [] } // first value of passenger without any bookings
+            )
+          )
+        )
+      )
+      .subscribe(({ passenger, bookings }) => {
+        socket.emit('passenger', { ...passenger.toObject(), bookings })
+      }),
     experiment.passengerUpdates.subscribe((passenger) =>
       socket.emit('passenger', passenger.toObject())
     ),
