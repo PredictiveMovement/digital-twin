242b219c28fdc1cd8ec8ab7062306f1863ae5bf0
Christian Landgren <christian@landgren.nu>
2022-10-29 10:22:09 +0200

    chore: move vroom specific code to vroom.js

--- packages/simulator/lib/dispatch/taxiDispatch.js
+++ packages/simulator/lib/dispatch/taxiDispatch.js
@@ -1,47 +1,5 @@
-const { toArray, map, filter, mergeMap, take } = require('rxjs/operators')
-const { plan } = require('../vroom')
+const { plan, taxiToVehicle, bookingToShipment } = require('../vroom')
 const moment = require('moment')
-
-const bookingToShipment = ({ id, pickup, destination }, i) => {
-  return {
-    id: i,
-    description: id,
-    amount: [1],
-    pickup: {
-      time_windows: pickup.departureTime?.length
-        ? [
-            [
-              moment(pickup.departureTime, 'hh:mm:ss').unix(),
-              moment(pickup.departureTime, 'hh:mm:ss').add(5, 'minutes').unix(),
-            ],
-          ]
-        : undefined,
-      id: i,
-      location: [pickup.position.lon, pickup.position.lat],
-    },
-    delivery: {
-      id: i,
-      location: [destination.position.lon, destination.position.lat],
-      time_windows: destination.arrivalTime?.length
-        ? [
-            [
-              moment(destination.arrivalTime, 'hh:mm:ss').unix(),
-              moment(destination.arrivalTime, 'hh:mm:ss')
-                .add(5, 'minutes')
-                .unix(),
-            ],
-          ]
-        : undefined,
-    },
-  }
-}
-const taxiToVehicle = ({ id, position, capacity, heading, bookings }, i) => ({
-  id: i,
-  description: id,
-  capacity: [capacity],
-  start: [position.lon, position.lat],
-  end: heading ? [heading.lon, heading.lat] : undefined,
-})
 
 const taxiDispatch = async (taxis, bookings) => {
   const vehicles = taxis.map(taxiToVehicle)
@@ -63,6 +21,27 @@ const taxiDispatch = async (taxis, bookings) => {
   })
 }
 
+const findBestRouteToPickupBookings = async (taxi, bookings) => {
+  const vehicles = [taxiToVehicle(taxi, 0)]
+  const shipments = bookings.map(bookingToShipment)
+
+  const result = await plan({ shipments, vehicles })
+
+  return result.routes[0]?.steps
+    .filter(({ type }) => ['pickup', 'delivery'].includes(type))
+    .map(({ id, type, arrival, departure }) => {
+      const booking = bookings[id]
+      const instruction = {
+        action: type === 'pickup' ? 'Pickup' : 'Delivery',
+        arrival,
+        departure,
+        booking,
+      }
+      return instruction
+    })
+}
+
 module.exports = {
   taxiDispatch,
+  findBestRouteToPickupBookings,
 }
