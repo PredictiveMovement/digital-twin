3f5f31833497d76de87852d51b0c8e4d3417e278
Mikael Gråborg <mikael.graborg@iteam.se>
2022-11-02 19:25:53 +0100

    chore: add measurestations in own routes file

--- packages/simulator/web/routes/measureStations.js
+++ packages/simulator/web/routes/measureStations.js
@@ -2,6 +2,9 @@ const { toArray } = require('rxjs')
 
 const register = (experiment, socket) => {
   return [
+    experiment.measureStationUpdates.subscribe((measurement) =>
+      io.emit('measureStationUpdates', measurement)
+    ),
     experiment.measureStations.pipe(toArray()).subscribe((measureStations) => {
       socket.emit('measureStations', measureStations)
     }),
