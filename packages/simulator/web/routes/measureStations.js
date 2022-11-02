5fa846a8b1c6bd59e18c3cc6c080d3085f98dac3
Mikael Gråborg <mikael.graborg@iteam.se>
2022-11-02 19:27:07 +0100

    fix: error in routes/measureStations

--- packages/simulator/web/routes/measureStations.js
+++ packages/simulator/web/routes/measureStations.js
@@ -3,7 +3,7 @@ const { toArray } = require('rxjs')
 const register = (experiment, socket) => {
   return [
     experiment.measureStationUpdates.subscribe((measurement) =>
-      io.emit('measureStationUpdates', measurement)
+      socket.emit('measureStationUpdates', measurement)
     ),
     experiment.measureStations.pipe(toArray()).subscribe((measureStations) => {
       socket.emit('measureStations', measureStations)
