683e7f48a0d1e82cc01cd472a9a2af0066754ae3
Christian Landgren <christian@landgren.nu>
2022-11-25 19:14:52 +0100

    refactor: refactored zip to the same structure as box

--- packages/sampleAddresses/routes/zip.js
+++ packages/sampleAddresses/routes/zip.js
@@ -3,59 +3,66 @@ const app = express()
 const fetch = require('node-fetch')
 const assert = require('assert')
 
-app.get('/:zipnr', (req, res) => {
-  const seed = req.query.seed || 1337
-  const zipnr = req.params.zipnr
-  const size = req.query.size || 10
-  assert(size <= 10000, 'Maximum size 10000')
-
-  const peliasHostname = process.env.PELIAS_HOSTNAME || 'localhost:9200'
-  const url = `http://${peliasHostname}/pelias/_search`
-  const query = {
-    query: {
-      function_score: {
-        query: {
-          wildcard: {
-            'address_parts.zip': zipnr,
-          },
-        },
-        random_score: {
-          seed: seed,
-          field: '_seq_no',
+const query = (zipnr, seed, size) => ({
+  query: {
+    function_score: {
+      query: {
+        wildcard: {
+          'address_parts.zip': zipnr,
         },
       },
+      random_score: {
+        seed: seed,
+        field: '_seq_no',
+      },
     },
-    size: size,
-  }
+  },
+  size: size,
+})
 
-  fetch(url, {
+const fetchAdresses = async (zipnr, seed, size) => {
+  const peliasHostname = process.env.PELIAS_HOSTNAME || 'localhost:9200'
+  const url = `http://${peliasHostname}/pelias/_search`
+
+  const json = await fetch(url, {
     method: 'POST',
-    body: JSON.stringify(query),
+    body: JSON.stringify(query(zipnr, seed, size)),
     headers: { 'Content-Type': 'application/json' },
-  })
-    .then((res) => res.json())
-    .then((json) => {
-      if (json.error) {
-        throw new Error(json.error)
-      }
-      const hits = json.hits.hits
-      const addresses = hits
-        .map((hit) => hit)
-        .map(
-          ({
-            _id: id,
-            _source: { center_point: position, address_parts: address },
-          }) => ({
-            address,
-            position,
-            id,
-          })
-        )
+  }).then((res) => res.json())
+
+  if (json.error) {
+    console.log('elastic error', json.error)
+    throw new Error('Error in database query')
+  }
+  const hits = json.hits.hits
+  const addresses = hits
+    .map((hit) => hit)
+    .map(
+      ({
+        _id: id,
+        _source: { center_point: position, address_parts: address },
+      }) => ({
+        address,
+        position,
+        id,
+      })
+    )
+  return addresses
+}
+
+app.get('/:zipnr', (req, res) => {
+  const seed = req.query.seed || 1337
+  const zipnr = +req.params.zipnr
+  const size = req.query.size || 10
+  assert(size <= 10000, 'Maximum size 10000')
+  assert(zipnr, 'Parameter: zipnr is required')
+
+  fetchAdresses(zipnr, seed, size)
+    .then((addresses) => {
       res.json(addresses)
     })
-    .catch((error) => {
-      console.log(error)
-      res.status(500).json(error.message)
+    .catch((err) => {
+      res.status(500).json({ error: err.message })
     })
 })
 
