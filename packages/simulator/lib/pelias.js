c3fcc220f09238adfee4e3758602aa614f9635be
Christian Landgren <christian@landgren.nu>
2022-12-26 22:08:33 +0100

    chore: replace old urls

--- packages/simulator/lib/pelias.js
+++ packages/simulator/lib/pelias.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch')
-const peliasUrl = process.env.PELIAS_URL || 'https://pelias.iteamdev.io'
+const peliasUrl =
+  process.env.PELIAS_URL || 'https://pelias.predictivemovement.se'
 
 console.log('Pelias URL', peliasUrl)
 module.exports = {
@@ -7,13 +8,20 @@ module.exports = {
     const url = `${peliasUrl}/v1/reverse?point.lat=${position.lat}&point.lon=${position.lon}&size=1&layers=address,venue`
     const promise = fetch(url)
       .then((response) => {
-        if (!response.ok) throw "pelias error: " + response.statusText
+        if (!response.ok) throw 'pelias error: ' + response.statusText
         return response.json()
       })
-      .then(p => p.features[0].geometry?.coordinates?.length ? p : Promise.reject('No coordinates found'))
+      .then((p) =>
+        p.features[0].geometry?.coordinates?.length
+          ? p
+          : Promise.reject('No coordinates found')
+      )
       .then(({ features: [{ geometry, properties } = {}] = [] }) => ({
         ...properties,
-        position: { lon: geometry.coordinates[0], lat: geometry.coordinates[1] },
+        position: {
+          lon: geometry.coordinates[0],
+          lat: geometry.coordinates[1],
+        },
       }))
 
     return promise
