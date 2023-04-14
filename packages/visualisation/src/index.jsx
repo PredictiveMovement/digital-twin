7e63e27d16d8b4fd2e9c83667a00610cf4fbb12b
Christian Landgren <christian@landgren.nu>
2023-04-14 17:58:25 +0200

    fix: remove unused libs

--- packages/visualisation/src/index.jsx
+++ packages/visualisation/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-// import reportWebVitals from './reportWebVitals'
 import { SocketIOProvider } from './context/socketIOProvider'
 import 'moment-duration-format'
 import { ThemeProvider, createTheme } from '@mui/material'
@@ -41,8 +40,3 @@ ReactDOM.render(
   </SocketIOProvider>,
   document.getElementById('root')
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
