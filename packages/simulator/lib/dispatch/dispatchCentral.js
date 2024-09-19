9ddc5cd7388056640f5621d8ee1ee11fd093f3e5
Simon Gustafsson <simon.gustafsson@iteam.se>
2024-09-19 18:11:44 +0200

    refactor: Remove limit from telge.js and update dispatchCentral.js

--- packages/simulator/lib/dispatch/dispatchCentral.js
+++ packages/simulator/lib/dispatch/dispatchCentral.js
@@ -1,18 +1,27 @@
 const { from } = require('rxjs')
 const { filter, mergeMap } = require('rxjs/operators')
 
-const handleBooking = (car) => (source) => {
+const filterUniqueBooking = (car) => (source) => {
   return source.pipe(
-    filter((booking) => !car.queue.find((b) => b.id === booking.id)),
-    filter((booking) => car.canHandleBooking(booking)),
-    mergeMap((booking) => car.handleBooking(booking))
+    filter((booking) => !car.queue.find((b) => b.id === booking.id))
   )
 }
+
+const filterCanHandleBooking = (car) => (source) => {
+  return source.pipe(filter((booking) => car.canHandleBooking(booking)))
+}
+
+const processBooking = (car) => (source) => {
+  return source.pipe(mergeMap((booking) => car.handleBooking(booking)))
+}
+
 const dispatch = (cars, bookings) => {
   return from(cars).pipe(
     mergeMap((car) =>
       from(bookings).pipe(
-        handleBooking(car)
+        filterUniqueBooking(car),
+        filterCanHandleBooking(car),
+        processBooking(car)
       )
     )
   )
