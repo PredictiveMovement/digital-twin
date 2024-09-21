cd722e277af196aafb0a4547f7d712cf9d575a9a
Christian Landgren <christian@landgren.nu>
2024-09-21 19:45:19 +0200

    fix: correct color opacity for 'Picked up' status in Map component

--- packages/visualisation/src/Map.jsx
+++ packages/visualisation/src/Map.jsx
@@ -192,9 +192,9 @@ const Map = ({
       { status } // TODO: Different colors for IKEA & HM
     ) =>
       status === 'Delivered'
-        ? [170, 255, 187, 128] // Set opacity to around 50 for delivered items
-        : status === 'Picked up'
         ? [170, 187, 255, 55]
+        : status === 'Picked up'
+        ? [170, 255, 187, 128] // Set opacity to around 50 for delivered items
         : [255, 170, 187, 55],
     pickable: true,
     onHover: ({ object, x, y, viewport }) => {
